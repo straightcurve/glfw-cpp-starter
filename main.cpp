7ae948ca667fc2f2a9feaf7937832a3b8741b698
2B <antoniu.ilea@gmail.com>
2021-01-09 06:59:43 +0200

    Updated build configuration

--- main.cpp
+++ main.cpp
@@ -1,23 +1,75 @@
-#include "vendor/glad/include/glad/glad.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-bool set_up();
-void tear_down();
+#include <stdio.h>
 
-int main() {
-    if (!set_up())
-        std::cout << "Initialization failed." << std::endl;
+namespace
+{
+    void errorCallback(int error, const char *description)
+    {
+        fprintf(stderr, "GLFW error %d: %s\n", error, description);
+    }
 
-    tear_down();
+    GLFWwindow *initialize()
+    {
+        int glfwInitRes = glfwInit();
+        if (!glfwInitRes)
+        {
+            fprintf(stderr, "Unable to initialize GLFW\n");
+            return nullptr;
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+        GLFWwindow *window = glfwCreateWindow(1280, 720, "ECS", nullptr, nullptr);
+        if (!window)
+        {
+            fprintf(stderr, "Unable to create GLFW window\n");
+            glfwTerminate();
+            return nullptr;
+        }
+
+        glfwMakeContextCurrent(window);
+
+        int gladInitRes = gladLoadGL();
+        if (!gladInitRes)
+        {
+            fprintf(stderr, "Unable to initialize glad\n");
+            glfwDestroyWindow(window);
+            glfwTerminate();
+            return nullptr;
+        }
+
+        return window;
+    }
+} // namespace
+
+int main(int argc, char *argv[])
+{
+    std::cout << "wtf";
+    glfwSetErrorCallback(errorCallback);
+
+    GLFWwindow *window = initialize();
+    if (!window)
+        return 1;
+
+    // Set the clear color to a nice green
+    glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
-}
-
-bool set_up() {
-    return glfwInit();
-}
-
-void tear_down() {
-    glfwTerminate();
 }
